9f1bd79dc61fb1136b7e79810211c3452d81b3a0
LUISCDF <luiscdf@ua.pt>
2019-11-07 19:00:45 +0000

    i wanna give up... on life

---
+++ teste.py
@@ -0,0 +1,16 @@
+class BitStream():
+    def __init__(self):
+        pass
+
+    def readBit(self):
+        pass
+
+    def writeBit(self):
+        pass
+
+    def readbyte(self):
+        pass
+
+    def writeByte(self):
+        pass
+
