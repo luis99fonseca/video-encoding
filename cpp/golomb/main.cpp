bd2011dc684d0dfb6fad3524e906c2cca25765e8
Tiago Mendes <tiagocmendes@ua.pt>
2019-12-05 16:56:17 +0000

    Finished golomb tests

--- cpp/golomb/main.cpp
+++ cpp/golomb/main.cpp
@@ -5,13 +5,20 @@
 
 using namespace std;
 
-bool unit_test(Golomb golomb, int n, vector<int> output)
+bool unit_encode_test(Golomb golomb, int n, vector<int> output)
 {
     vector<int> encoded = golomb.encode(n);
 
     for(int i = 0; i < encoded.size(); i++) 
         assert(encoded.at(i) == output.at(i));
         
+    return true;
+}
+
+bool unit_decode_test(Golomb golomb, vector<int> code, int output)
+{
+    int n = golomb.decode(code);
+    assert(n == output);
     return true;
 }
 
@@ -44,32 +51,50 @@ int main()
     };
 
     for(int code = 0; code < codes.size(); code++) 
-        unit_test(golomb,code,codes.at(code));
+        unit_encode_test(golomb,code,codes.at(code));
 
     cout << ("------------------------------") << "\n";
     cout << ("SECOND TEST - DECODE WITH M = 4") << "\n";
     cout << ("------------------------------") << "\n";
 
-    vector<vector<int>> codes = { 
-        {0,0}, 
-        {0,1}, 
-        {0,1,0}, 
-        {0,1,1}, 
-        {1,0,0}, 
-        {1,0,1}, 
-        {1,0,1,0}, 
-        {1,0,1,1}, 
-        {1,1,0,0}, 
-        {1,1,0,1},
-        {1,1,0,1,0},
-        {1,1,0,1,1},
-        {1,1,1,0,0},
-        {1,1,1,0,1},
-        {1,1,1,0,1,0},
-        {1,1,1,0,1,1}
-    };
-    
     for(int code = 0; code < codes.size(); code++) 
-        unit_test(golomb,code,codes.at(code));
+        unit_decode_test(golomb,codes.at(code),code);
+    
+
+    cout << ("------------------------------") << "\n";
+    cout << ("FIRST TEST - ENCODE WITH M = 5") << "\n";
+    cout << ("------------------------------") << "\n";
+
+    golomb.set_m(5);
+
+    vector<vector<int>> codes2 = { 
+        {0,0,0}, 
+        {0,0,1}, 
+        {0,1,0}, 
+        {0,1,1,0}, 
+        {0,1,1,1}, 
+        {1,0,0,0}, 
+        {1,0,0,1}, 
+        {1,0,1,0}, 
+        {1,0,1,1,0}, 
+        {1,0,1,1,1},
+        {1,1,0,0,0},
+        {1,1,0,0,1},
+        {1,1,0,1,0},
+        {1,1,0,1,1,0},
+        {1,1,0,1,1,1},
+        {1,1,1,0,0,0}
+    };
+
+
+    for(int code = 0; code < codes2.size(); code++) 
+        unit_encode_test(golomb,code,codes2.at(code));
+
+    cout << ("------------------------------") << "\n";
+    cout << ("SECOND TEST - DECODE WITH M = 4") << "\n";
+    cout << ("------------------------------") << "\n";
+
+    for(int code = 0; code < codes2.size(); code++) 
+        unit_decode_test(golomb,codes2.at(code),code);
 
 }
