a0db00f17d0bcbc154b63a84b1b0b1895d83f9a0
Tiago Mendes <tiagocmendes@ua.pt>
2019-12-05 12:34:07 +0000

    Started converting cpp tests

--- cpp/golomb/main.cpp
+++ cpp/golomb/main.cpp
@@ -1,18 +1,75 @@
 #include <iostream>
 #include <vector>
+#include <assert.h>
 #include "golomb.cpp"
 
 using namespace std;
+
+bool unit_test(Golomb golomb, int n, vector<int> output)
+{
+    vector<int> encoded = golomb.encode(n);
+
+    for(int i = 0; i < encoded.size(); i++) 
+        assert(encoded.at(i) == output.at(i));
+        
+    return true;
+}
+
+
 int main()
 {   
-    Golomb golomb(5);
-    
-    for(int c = 0; c <= 15; c++) {
-        vector<int> code = golomb.encode(c);
-        int decimal = golomb.decode(code);
-        cout << decimal << "\n";
-    }
-    
+    Golomb golomb(4);
 
+    cout << ("------------------------------") << "\n";
+    cout << ("FIRST TEST - ENCODE WITH M = 4") << "\n";
+    cout << ("------------------------------") << "\n";
+
+    vector<vector<int>> codes = { 
+        {0,0}, 
+        {0,1}, 
+        {0,1,0}, 
+        {0,1,1}, 
+        {1,0,0}, 
+        {1,0,1}, 
+        {1,0,1,0}, 
+        {1,0,1,1}, 
+        {1,1,0,0}, 
+        {1,1,0,1},
+        {1,1,0,1,0},
+        {1,1,0,1,1},
+        {1,1,1,0,0},
+        {1,1,1,0,1},
+        {1,1,1,0,1,0},
+        {1,1,1,0,1,1}
+    };
+
+    for(int code = 0; code < codes.size(); code++) 
+        unit_test(golomb,code,codes.at(code));
+
+    cout << ("------------------------------") << "\n";
+    cout << ("SECOND TEST - DECODE WITH M = 4") << "\n";
+    cout << ("------------------------------") << "\n";
+
+    vector<vector<int>> codes = { 
+        {0,0}, 
+        {0,1}, 
+        {0,1,0}, 
+        {0,1,1}, 
+        {1,0,0}, 
+        {1,0,1}, 
+        {1,0,1,0}, 
+        {1,0,1,1}, 
+        {1,1,0,0}, 
+        {1,1,0,1},
+        {1,1,0,1,0},
+        {1,1,0,1,1},
+        {1,1,1,0,0},
+        {1,1,1,0,1},
+        {1,1,1,0,1,0},
+        {1,1,1,0,1,1}
+    };
     
+    for(int code = 0; code < codes.size(); code++) 
+        unit_test(golomb,code,codes.at(code));
+
 }
